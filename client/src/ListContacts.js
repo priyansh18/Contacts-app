95a9bb4c6a083aa964190057609785b7f2d523cb
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-06-23 18:40:37 +0530

    Passing data into components with props

---
+++ client/src/ListContacts.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+class ListContacts extends Component {
+  // state = {  }
+  render() {
+    const { contacts } = this.props;
+    console.log("Contacts", contacts);
+    return (
+      <ol className="contact-list">
+        {contacts.map((contact) => (
+          <li key={contact.id} className="contact-list-item">
+            <div
+              className="contact-avatar"
+              style={{ backgroundImage: `url(${contact.avatarURL})` }}
+            />
+            <div className="contact-details">
+              <p>{contact.name}</p>
+              <p>{contact.handle}</p>
+            </div>
+            <button className="contact-remove">Remove</button>
+          </li>
+        ))}
+      </ol>
+    );
+  }
+}
+
+export default ListContacts;
