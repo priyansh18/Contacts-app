a093f8bf18260fc55e3cc2b181547df1b2032cdb
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-06-24 17:26:32 +0530

    Querying & Searching input field

--- client/src/ListContacts.js
+++ client/src/ListContacts.js
@@ -2,31 +2,58 @@ import React, { Component } from "react";
 import { PropTypes } from "prop-types";
 
 class ListContacts extends Component {
-  // state = {  }
+  state = {
+    query: "",
+  };
+
+  updateQuery = (query) => {
+    this.setState(() => ({
+      query: query.trim(),
+    }));
+  };
   render() {
     const { contacts, onDelete } = this.props;
-    console.log("Contacts", contacts);
+    const { query } = this.state;
+    // console.log("Contacts", contacts);
+    const showingContacts =
+      query === ""
+        ? contacts
+        : contacts.filter((c) =>
+            c.name.toLowerCase().includes(query.toLowerCase())
+          );
+
     return (
-      <ol className="contact-list">
-        {contacts.map((contact) => (
-          <li key={contact.id} className="contact-list-item">
-            <div
-              className="contact-avatar"
-              style={{ backgroundImage: `url(${contact.avatarURL})` }}
-            />
-            <div className="contact-details">
-              <p>{contact.name}</p>
-              <p>{contact.handle}</p>
-            </div>
-            <button
-              className="contact-remove"
-              onClick={() => onDelete(contact)}
-            >
-              Remove
-            </button>
-          </li>
-        ))}
-      </ol>
+      <div className="list-contacts">
+        <div className="list-contacts-top">
+          <input
+            className="search-contacts"
+            type="text"
+            placeholder="Search Contacts"
+            value={this.state.query}
+            onChange={(event) => this.updateQuery(event.target.value)}
+          />
+        </div>
+        <ol className="contact-list">
+          {showingContacts.map((contact) => (
+            <li key={contact.id} className="contact-list-item">
+              <div
+                className="contact-avatar"
+                style={{ backgroundImage: `url(${contact.avatarURL})` }}
+              />
+              <div className="contact-details">
+                <p>{contact.name}</p>
+                <p>{contact.handle}</p>
+              </div>
+              <button
+                className="contact-remove"
+                onClick={() => onDelete(contact)}
+              >
+                Remove
+              </button>
+            </li>
+          ))}
+        </ol>
+      </div>
     );
   }
 }
